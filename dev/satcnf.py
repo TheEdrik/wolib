21d10c5a6e3fc9d00a99d881be801fda8ea50989
Tamas Visegrady <tamas.visegrady@gmail.com>
2023-03-05 18:37:32 +0100

    add: SAT: AND, OR encoder; skeleton of A<=MAX comparison

--- dev/satcnf.py
+++ dev/satcnf.py
@@ -2,7 +2,7 @@
 
 ## construct SAT-solver expressions for solving vehicle routing subproblems
 
-import re
+import re, sys
 
 
 ##-----------------------------------------
@@ -41,6 +41,225 @@ def satsolv_strings2ints(vars, values=None, first=1):
 				res[ id ] = first
 				first += 1
 	return res
+
+
+##-----------------------------------------
+sNALL0 = 'NZ'                     ## suffix for not-all-(values-)zero +variable
+sNALL1 = 'ALL1'                   ## suffix for all-(values-)one +variable
+
+
+##-----------------------------------------
+## sample: A; B; N = A | B
+##     1) A | B | not(N)             N -> (A | B)
+##     2) not(A) | N       together: (A | B) -> N
+##     3) not(B) | N
+##
+## returns list of clauses + comment
+##
+def satsolv_or(base, vars):
+	cls = []
+	v   = sorted(vars)
+	nz  = base + sNALL0
+
+	all = list((base +b)  for b in  v)
+	all.append(f'-{ nz }')
+		##
+	cls.append( " ".join(all) )                   ## A | B | not(N)
+
+	terms = list((base +b)  for b in v)
+
+	cls.extend((f'-{ t } { nz }')  for t in terms)
+
+	return cls, f'{ nz } := (' +(" OR ".join(terms)) +')'
+
+
+##-----------------------------------------
+## sample: A; B; N = A & B
+##     1) not(A) | not(B) | N            N -> (A & B)
+##     2) A | not(N)           together: (A & B) -> N
+##     3) B | not(N)
+##
+## returns list of clauses + comment
+##
+def satsolv_and(base, vars):
+	cls  = []
+	v    = sorted(vars)
+	all1 = base + sNALL1
+
+	all = list((f"-{ (base +b) }")  for b in  v)
+	all.append(all1)
+		##
+	cls.append( " ".join(all) )                   ## not(A) | not(B) | N
+
+	terms = list((base +b)  for b in v)
+
+	cls.extend((f'{ t } -{ all1 }')  for t in terms)
+
+	return cls, f'{ all1 } := (' +(" AND ".join(terms)) +')'
+
+
+
+##----------------------------------------------------------------------------
+## input restricted to string of all-0/1 integers of value V
+##
+## return broken down to consecutive regions of all-identical elements
+##     '11010' -> [ '11', '0', '1', '0' ]
+##
+def arr2consective(vbin):
+	arr = [ ch.group(0)  for ch in  re.finditer(r'(\d)\1*', vbin) ]
+
+	return arr
+
+
+##--------------------------------------
+## decomposed versions for small N, with don't care trailing bits marked:
+##
+##                               v--- nr of comparison windows if >0
+##    i= 1  ib=    1  ib'=    1           -- never above
+##    i= 2  ib=   10  ib'=   10  2
+##    i= 3  ib=   11  ib'=   11           -- never above
+##    i= 4  ib=  100  ib'=  100  2
+##    i= 5  ib=  101  ib'=  10.  2
+##    i= 6  ib=  110  ib'=  110  2
+##    i= 7  ib=  111  ib'=  111           -- never above
+##    i= 8  ib= 1000  ib'= 1000  2
+##    i= 9  ib= 1001  ib'= 100.  2
+##    i=10  ib= 1010  ib'= 1010  4
+##    i=11  ib= 1011  ib'= 10..  2
+##    i=12  ib= 1100  ib'= 1100  2
+##    i=13  ib= 1101  ib'= 110.  2
+##    i=14  ib= 1110  ib'= 1110  2
+##    i=15  ib= 1111  ib'= 1111           -- never above
+##    i=16  ib=10000  ib'=10000  2
+##    i=17  ib=10001  ib'=1000.  2
+##    i=18  ib=10010  ib'=10010  4
+##    i=19  ib=10011  ib'=100..  2
+##    i=20  ib=10100  ib'=10100  4
+##    i=21  ib=10101  ib'=1010.  4
+##    i=22  ib=10110  ib'=10110  4
+##    i=23  ib=10111  ib'=10...  2
+##    i=24  ib=11000  ib'=11000  2
+##    i=25  ib=11001  ib'=1100.  2
+##    i=26  ib=11010  ib'=11010  4
+##    i=27  ib=11011  ib'=110..  2
+##    i=28  ib=11100  ib'=11100  2
+##    i=29  ib=11101  ib'=1110.  2
+##    i=30  ib=11110  ib'=11110  2
+##    i=31  ib=11111  ib'=11111      -- never above
+
+
+##--------------------------------------
+## N-bit variable list, assumed big-endian V value as v0..v[N-1]
+## construct expression for ...N-bit uint... <= maxv
+##
+## returns (control variable, list of clauses (possibly empty), comment)
+##     None control variable AND empty clause -> no comparison needed
+##
+## TODO: restricted to fixed compositions
+## sanity check that maxv is has minimal nr. of bits
+##
+def satsolv_le(vars, maxv):
+	cls   = []
+	mvbin = f'{ maxv :b}'           ## implicit 0-stripping, so [0]==1
+
+	if len(vars) != len(mvbin):
+		raise ValueError("CNF: inconsistent <= set size")
+
+	arr = arr2consective(mvbin)
+	assert(arr[0][0] == '1')        ## see above, 1st region always all-0
+
+	if len(arr) == 1:
+		return None, [], ''       ## 2^N -1:  V is never > 2^N-1
+
+				## '11011' -> [ '11', '0', '11' ]
+				## value is always >= MS region, which is all-1
+				##
+				## hierarchical decoding:
+				##   - all-1 region: is value < mask?
+				##       ->  value NOT all-1 (AND)  ->  V < L
+				##   - all-0 region: is value > mask?
+				##       ->  value not all-0 (OR)  ->  V > L
+				##   - any other case: proceed to lower-valued
+				##     bits
+				##
+				## regions alternate
+				##
+				## single-bit regions trivially simplify,
+				## use individual bits and skip AND/OR
+
+				## trailing all-1 region may be dropped, since
+				## it never decides: V <= L  against all-1 L ->
+				## higher-order bits must decide before
+	if (arr[-1][0] == '1'):
+		arr.pop(-1)
+				## -> nr of regions to compare is even
+				## (1 -> 0 -> ... -> 1)  was even, trailing
+				##  1 has been dropped
+
+	if (len(arr) > 3):
+		raise ValueError("check back when implemented...")
+
+	bdone = 0               ## 'bits done': processed, MS to LS
+	bvars = vars[:]         ## allow modifying local list
+
+	for r in arr:
+		currbits = len(r)
+		currvars, bvars = bvars[ :currbits ], bvars[ currbits: ]
+
+		if currbits == 1:
+						## ...single-bit term...
+			pass
+
+		else:
+						## ...process rest...
+			pass
+
+		bdone += currbits
+
+	return None, cls, ''
+
+
+##-----------------------------------------
+## given 2x N IDs (for two N-bit IDs) and a max-element count, return
+## clauses which encode True if:
+##   (1) any of the N-bit inputs is all-zero
+##   (2) the N-bit unsigned ints differ
+## in other words, 'nonzero-differ(v1, v2)'
+##
+## returns (list of clauses); comment
+##
+## encode only to strings, require subsequent string-to-variable(index) mapping
+##
+## comparison is used when cross-checking assigned times, each 'vars' is:
+##   dXX tYY z0..z(N-1)    delivery XX, time window YY, vehicle ZZ delivers
+##                         -> delivery constraint is: if both deliveries
+##                            are assigned, MUST NOT be the same vehicle
+##                            (since they could not traverse in time)
+##
+## 'base' stores dXX tYY terms, or any common prefix
+## define 'base' as variable if any of the 'base+vars' bits are True
+##
+def satsolv_nzdiffer_n(base1, base2, vars1, vars2=[]):
+	cls = []
+
+	v1 = vars1[:]
+
+	if vars2 == []:
+		v2 = v1
+	else:
+		if len(v1) != len(v2):
+			raise ValueError(f"mismatched sizes ( {len(v1) } " +
+				f" != { len(v2) })")
+
+	cls, _ = satsolv_or(base1, v1)
+	cls.extend( satsolv_or(base2, v2)[0] )
+
+	for c in cls:
+		print(f'  CLAUSE={ c }')
+
+## TODO: comparison
+
+	return cls, ''
 
 
 ##-----------------------------------------
@@ -126,5 +345,15 @@ def nof1(n):
 
 ##-----------------------------------------------------------
 if __name__ == '__main__':
+	print(satsolv_and("base", [ "A", "B", "C", ]))
+	print(satsolv_le([ "v0", "v1", "v2", "v3", ], 15))
+	print(satsolv_le([ "v0", "v1", "v2", "v3", ], 9))
+	print(satsolv_le([ "v0", "v1", "v2", "v3", "v4", ], 23))
+	sys.exit(0)
+
+	print(satsolv_or("base", [ "A", "B", ]))
+	print(satsolv_nzdiffer_n("d31t11", "d44t22",
+	      	             [ "z0", "z1", "z2", "z3", "z4" ]))
+
 	nof1(256)
 
